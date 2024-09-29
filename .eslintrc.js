1c04869b2d0328b61d08e8cda21e8c25785df634
Ganesh Ramdhan <73478034+GRamdhan@users.noreply.github.com>
2024-09-29 19:16:15 +0200

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true,
+    },
+    extends: [
+      'plugin:vue/vue3-essential',
+      'eslint:recommended',
+    ],
+    parserOptions: {
+      parser: '@babel/eslint-parser',
+      requireConfigFile: false, // Voeg deze regel toe als je geen Babel-configuratiebestand hebt
+    },
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'vue/valid-v-for': 'error',
+      'vue/valid-v-bind': 'error',
+    },
+  };
+  
